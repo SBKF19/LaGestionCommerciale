b9b7ef50e40a43e5b2478b7247d24991defbd545
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-11-28 15:15:47 +0100

    mise en place de l'affichage des devis

---
+++ LaGestionCommerciale/FrmDevis.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaGestionCommerciale
+{
+    public partial class FrmDevis : Form
+    {
+        public FrmDevis()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvClient_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnAddDevis_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDetail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbStatut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlDevis_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblTVA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblRemiseGlobale_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numTVA_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numRemiseGlobale_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblProduits_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
