256ab577d4e838607cbfc23397996b7882e1436e
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2025-12-04 12:25:11 +0100

    Preparation du bouton supprimer et modifier

---
+++ LaGestionCommerciale/FrmDevisModifSup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaGestionCommerciale
+{
+    public partial class FrmDevisModifSup : Form
+    {
+        public FrmDevisModifSup()
+        {
+            InitializeComponent();
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmDevisModifSup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
